4201f0c565452e4be0123dc94db4ec682f69bb82
LeeJaeHoon43 <ljw160791@gmail.com>
2024-01-18 17:46:04 +0900

    MembershipServiceApplication 명칭 변경

--- membership-service/src/main/java/com/mypay/membership/MembershipServiceApplication.java
+++ membership-service/src/main/java/com/mypay/membership/MembershipServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MembershipApplication {
+public class MembershipServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MembershipApplication.class, args);
+        SpringApplication.run(MembershipServiceApplication.class, args);
     }
 
 }
